5a3a721621f604c3a946a982858e3fec9ed21ba1
victor.teixido <victor.teixido@est.fib.upc.edu>
2023-05-18 12:09:21 +0200

    code templates added

--- compressor.py
+++ compressor.py
@@ -1,12 +1,87 @@
 import sys
-from math import floor
-from collections import defaultdict
+from collections import Counter
+from operator import itemgetter
 
-###########################
-### ARITHMETIC ENCODING ###
-###########################
+#######################
+### BURROWS-WHEELER ###
+#######################
 
-# calculate the source from the given text
+def compare_idx(x, y, txt):
+    if txt[x] != txt[y]:
+        return txt[x] < txt[y]
+    
+    return compare_idx((x+1)%len(txt), (y+1)%len(txt), txt)
+
+def merge_sort(arr, txt):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    left = merge_sort(left, txt)
+    right = merge_sort(right, txt)
+
+    return merge(left, right, txt)
+
+
+def merge(left, right, txt):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if compare_idx(left[i], right[j], txt):
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+
+    return result
+
+def sorted_permutations(txt):
+    perms = list(range(len(txt)))
+    perms = merge_sort(perms, txt)
+    return perms
+
+def encode_burrows_wheeler_merge(txt):
+    sorted_perms = sorted_permutations(txt)
+    last_col = ''.join(txt[x-1] for x in sorted_perms)
+    pos = sorted_perms.index(0)
+
+    return last_col, pos
+
+
+#####################
+### MOVE TO FRONT ###
+#####################
+
+def encode_move_to_front(txt):
+    alphabet = list(set(txt))
+    alphabet.sort()
+    q = len(alphabet)
+    sequence = []
+    for char in txt:
+        index = alphabet.index(char)
+        sequence.append(index)
+        alphabet.pop(index)
+        alphabet.insert(0, char)
+    return sequence
+
+
+###############
+### HUFFMAN ###
+###############
+
 def source_fromtext(txt, n=1):
     freq_packs = {}
     for i in range(len(txt) - n + 1):
@@ -18,84 +93,204 @@ def source_fromtext(txt, n=1):
     freq_list = [(k, v) for k, v in freq_packs.items()]
     return sorted(freq_list, key=lambda x: x[0])
 
-# apply arithmetic encoding algorithm to the given text
-def arithmetic_encode_bin(txt,k):
-    src = source_fromtext(txt)
-    suma = sum([x[1] for x in src])
-    init_probs = [(x[0],x[1]/suma) for x in src]
-    cumulative_probs = [0] + [sum(p[1] for p in init_probs[:i+1]) for i in range(len(init_probs))]
+def kraft_inequality(lengths, q):
+    s = 0
+    for l in lengths:
+        s += q**-l
+
+    return s <= 1
+
+def format_to_alf(number, base, length, alf):
+    if number == 0:
+        res = alf[0]
+        if length == 1:
+            return res
+        else:
+            count = 1
+            digits = []
+            digits.append(res)
+            while (count < length):
+                digits.insert(0, alf[0])
+                count +=1
+            res = ''.join(str(e) for e in digits[::-1])
+            return res
+
+    digits = []
+    while number > 0:
+        digits.append(alf[int(number % base)])
+        number //= base
+    count = len(digits)
+    while (count < length):
+                digits.append(alf[0])
+                count +=1        
+    res = ''.join(str(e) for e in digits[::-1])
+    return res
+
+def canonical_code(L,q=2, alf = [0,1]):
+    if not kraft_inequality(L, q):
+        return 'The entry does not satisfy Kraft-McMillan inequality.'
     
-    alpha = '0' * k
-    beta = '1' * k
-    c = ""
-    u = 0
-    s = ""
+    bl_count = Counter(L)
+    code = 0
+    bl_count[0] = 0
+    next_code = {}
+    maximum = max(L) + 1       
+    for l in range (1, maximum):
+        code = (code + bl_count[l-1])*q
+        next_code[l] = code 
+    def_code = []
+    lengths = {}
+    for l in L:
+        length = l
+        def_code.append(next_code[length])
+        lengths[next_code[length]] = length
+        next_code[length] += 1
+    def_code = list(map(lambda x: format_to_alf(x,q,lengths[x], alf),def_code))
+    return def_code
 
-    for i in range(0, len(txt)):
-        s += txt[i]
-        if not any(s in tupla for tupla in init_probs):
-            continue
+def huffman_code(txt, src, package_size=1):
+    d_nodes = {}
+    for c in src:
+        d_nodes[c[0]] = 0
+    
+    sorted_d = sorted(src, key=lambda x: x[1]) 
 
-        delta = int(beta,2) - int(alpha,2) + 1
-        current_intervals = [(int(alpha,2) + int(floor(delta * cumulative_probs[j-1])),
-                              int(alpha,2) + int(floor(delta * cumulative_probs[j]) - 1)) 
-                             for j in range(1, len(cumulative_probs))]
+    while len(sorted_d) > 1:
+        new_c = sorted_d[0][0] + sorted_d[1][0]
+        new_f = sorted_d[0][1] + sorted_d[1][1]
         
-        pos = [x[0] for x in init_probs].index(s)
-        alpha = bin(current_intervals[pos][0])[2:].zfill(k)
-        beta = bin(current_intervals[pos][1])[2:].zfill(k)
-        
-        while alpha[0] == beta[0]:
-            c += alpha[0]
-            if alpha[0] == '0':
-                    c += '1' * u
-            else:
-                    c += '0' * u
-            u = 0
-            alpha = alpha[1:] + '0'
-            beta = beta[1:] + '1'
+        for i in range(0, len(sorted_d[0][0]), package_size):
+            package = sorted_d[0][0][i:i+package_size]
+            d_nodes[package] += 1
             
-        while alpha[:2] == '01' and beta[:2] == '10':
-            alpha = alpha[0] + alpha[2:] + '0'
-            beta = beta[0] + beta[2:] + '1'
-            u += 1
+        for i in range(0, len(sorted_d[1][0]), package_size):
+            package = sorted_d[1][0][i:i+package_size]
+            d_nodes[package] += 1
         
-        s = ""
-    return c + '1'
+        sorted_d[1] = (new_c,new_f)
+        sorted_d.pop(0)
+        sorted_d = sorted(sorted_d, key=lambda x: x[1])
+    
+    result = [(key,value) for key, value in zip(d_nodes.keys(), canonical_code(d_nodes.values(), 2, ['0','1']))]
+    return result
+
+
+##############
+### ENCODE ###
+##############
+
+def encode(txta,corr):
+    corr = dict(corr)
+    txt_encoded = ''
+    i, j = 0, 0
+    while j<=len(txta):
+        substring = txta[i:j]
+        if substring in corr:
+            txt_encoded += corr[substring]
+            i = j
+        j += 1
+        
+    if i != len(txta): # all the text could not be processed
+        return 'Message could not be encoded'
+    return txt_encoded
+
+
+#########################
+### SAVE FILE PROCESS ###
+#########################
+
+def write_coded_text_to_file(alp, src, max_digits, index, coded_txt):
+
+    f = open('coded_txt.txt','wb')
+
+    # Encode and write alp
+    # CODE HERE
+
+    # Encode and write src
+    # CODE HERE
+
+    # Encode and write max digits
+    # CODE HERE
+
+    # Encode and write index
+    # CODE HERE
+    
+    # Write coded text
+    f.write(coded_txt)
+    f.close()
+    #Hacer tantos writes como parámetros necesitemos que se guarden
+    #linea1 = "Linea1"
+    #linea1 = linea1 + '\n'
+    #linea2 = "Linea2"
+    #linea2 = linea2 + '\n'
+    #f.write(linea1)
+    #f.write(linea2)
+    #Importante añandir '\n' al final de cada uno pq si no, no se guarda en cada línea. Todo tiene que ser en string
+    #return coded_txt #Retorna el código para después calcular el rendimiento
+
+    '''Segunda opción, con esta no se añade explicitamente el '\n'
+    #Hace cosas
+    with open('coded_txt.txt','w') as f:
+        print("Linea 1", file=f)
+        print("Linea 2", file=f)
+    '''
+
 
 ###########################
 ### COMPRESSION PROCESS ###
 ###########################
 
 def compressor(txt):
-    # encode the txt
-    k = 64
-    coded_txt = arithmetic_encode_bin(txt,k)
 
-    # write the code into file
-    f = open('coded_txt.txt','w')
-    f.write(coded_txt)
-    f.close()
+    # Alphabet of text
+    alp = sorted(list(set(txt)))
+
+    # Burrows-Wheeler transform
+    bw = encode_burrows_wheeler_merge(txt)
+    bw_code = bw[0]
+    index = bw[1]
+
+    # Move-to-Front
+    mtf_code = encode_move_to_front(bw_code)
+
+    max_digits = len(str(max(mtf_code)))
+
+    mtf_code = [str(x).zfill(max_digits) for x in mtf_code]
+    mtf_code = ''.join(mtf_code)
+
+    # Huffman
+    src = source_fromtext(mtf_code,2)
+    huf = huffman_code(mtf_code, src, 2)
+    corr = dict(huf)
+
+    # Encoding
+    huf_code = encode(mtf_code,corr)
+    coded_txt = int(huf_code, 2).to_bytes((len(huf_code) + 7) // 8, byteorder='big')
+
+    # Write paramteres and coded text to file
+    write_coded_text_to_file(alp, src, max_digits, index, coded_txt)
 
     return coded_txt
 
+############
+### MAIN ###
+############
+
 def main():
-    # read input file as txt
+    # Read input file
     filename = sys.argv[1]
     f = open(filename,'r',encoding='utf-8')
     txt = f.readline()
     f.close()
 
-    # compress the input txt
+    # Encode text and write it to file
     coded_txt = compressor(txt)
 
-    # calculate the performance
+    # Calculate encoding performance
     unicode_chars = len(txt)
-    coded_bytes = len(coded_txt) / 8
-
+    coded_bytes = len(coded_txt)
     performance = (coded_bytes / unicode_chars) * 8
-
-    print("Performance of the compressing algorithm:", performance)
+    print('performance:',performance)
 
 if __name__ == "__main__":
     main()
---
+++ decompressor.py
@@ -0,0 +1,231 @@
+import sys
+from collections import Counter
+from operator import itemgetter
+
+#######################
+### BURROWS-WHEELER ###
+#######################
+
+def decode_burrows_wheeler(last_col, pos):
+    n = len(last_col)
+    first_col = sorted([(i, x) for i, x in enumerate(last_col)], key=itemgetter(1))
+    T = [None for i in range(n)]
+    for i, y in enumerate(first_col):
+        j, _ = y
+        T[j] = i
+    Tx = [pos]
+    for i in range(1, n):
+        Tx.append(T[Tx[i - 1]])
+    s = [last_col[i] for i in Tx]
+    s.reverse()
+    return ''.join(s)
+
+
+#####################
+### MOVE TO FRONT ###
+#####################
+
+def decode_move_to_front(cod, alp):
+    alphabet = list(alp)
+    txt = ""
+    for index in cod:
+        char = alphabet[index]
+        txt += char
+        alphabet.pop(index)
+        alphabet.insert(0, char)
+    return txt
+
+
+###############
+### HUFFMAN ###
+###############
+
+def source_fromtext(txt, n=1):
+    freq_packs = {}
+    for i in range(len(txt) - n + 1):
+        packs = txt[i:i+n]
+        if packs in freq_packs:
+            freq_packs[packs] += 1
+        else:
+            freq_packs[packs] = 1
+    freq_list = [(k, v) for k, v in freq_packs.items()]
+    return sorted(freq_list, key=lambda x: x[0])
+
+def kraft_inequality(lengths, q):
+    s = 0
+    for l in lengths:
+        s += q**-l
+
+    return s <= 1
+
+def format_to_alf(number, base, length, alf):
+    if number == 0:
+        res = alf[0]
+        if length == 1:
+            return res
+        else:
+            count = 1
+            digits = []
+            digits.append(res)
+            while (count < length):
+                digits.insert(0, alf[0])
+                count +=1
+            res = ''.join(str(e) for e in digits[::-1])
+            return res
+
+    digits = []
+    while number > 0:
+        digits.append(alf[int(number % base)])
+        number //= base
+    count = len(digits)
+    while (count < length):
+                digits.append(alf[0])
+                count +=1        
+    res = ''.join(str(e) for e in digits[::-1])
+    return res
+
+def canonical_code(L,q=2, alf = [0,1]):
+    if not kraft_inequality(L, q):
+        return 'The entry does not satisfy Kraft-McMillan inequality.'
+    
+    bl_count = Counter(L)
+    code = 0
+    bl_count[0] = 0
+    next_code = {}
+    maximum = max(L) + 1       
+    for l in range (1, maximum):
+        code = (code + bl_count[l-1])*q
+        next_code[l] = code 
+    def_code = []
+    lengths = {}
+    for l in L:
+        length = l
+        def_code.append(next_code[length])
+        lengths[next_code[length]] = length
+        next_code[length] += 1
+    def_code = list(map(lambda x: format_to_alf(x,q,lengths[x], alf),def_code))
+    return def_code
+
+def huffman_code(txt, src, package_size=1):
+    d_nodes = {}
+    for c in src:
+        d_nodes[c[0]] = 0
+    
+    sorted_d = sorted(src, key=lambda x: x[1]) 
+
+    while len(sorted_d) > 1:
+        new_c = sorted_d[0][0] + sorted_d[1][0]
+        new_f = sorted_d[0][1] + sorted_d[1][1]
+        
+        for i in range(0, len(sorted_d[0][0]), package_size):
+            package = sorted_d[0][0][i:i+package_size]
+            d_nodes[package] += 1
+            
+        for i in range(0, len(sorted_d[1][0]), package_size):
+            package = sorted_d[1][0][i:i+package_size]
+            d_nodes[package] += 1
+        
+        sorted_d[1] = (new_c,new_f)
+        sorted_d.pop(0);
+        sorted_d = sorted(sorted_d, key=lambda x: x[1])
+    
+    result = [(key,value) for key, value in zip(d_nodes.keys(), canonical_code(d_nodes.values(), 2, ['0','1']))]
+    return result
+
+
+##############
+### DECODE ###
+##############
+
+def decode(txtb,corr):
+    corr = dict(corr)
+    corr_keys = list(corr.keys())
+    corr_values = list(corr.values())
+    txt_decoded = ''
+    i, j = 0, 0
+    while j<=len(txtb):
+        substring = txtb[i:j]
+        if substring in corr_values:
+            pos = corr_values.index(substring)
+            txt_decoded += corr_keys[pos]
+            i = j
+        j += 1
+        
+    if i != len(txtb): # all the text could not be processed
+        return 'Message could not be decoded'
+    return txt_decoded
+
+
+#########################
+### SAVE FILE PROCESS ###
+#########################
+
+def write_decoded_text_to_file(decoded_txt):
+    f = open('decoded_txt.txt','w')
+    f.write(decoded_txt)
+    f.close()
+
+
+#######################################
+### GET PARAMETERS AND ENCODED TEXT ###
+#######################################
+
+def split_txt(txt):
+    alp = ""
+    src = ""
+    max_digits = ""
+    index = ""
+    coded_txt = ""
+
+    # CODE HERE
+
+    return alp, src, max_digits, index, coded_txt
+
+
+#############################
+### DECOMPRESSION PROCESS ###
+#############################
+
+def decompressor(input_txt):
+
+    # Read encoded text and parameters
+    alp, src, max_digits, index, coded_txt = split_txt(input_txt)
+
+    # Convert from bytes to string
+    # CODE HERE
+
+    # Huffman
+    huf = huffman_code(coded_txt,src,2)
+    corr = dict(huf)
+
+    # Decode
+    huf_decoded = decode(coded_txt,corr)
+    huf_decoded = [int((huf_decoded[i:i+max_digits])) for i in range(0, len(huf_decoded), max_digits)]
+
+    # Move-to-Front
+    mtf_decoded = decode_move_to_front(huf_decoded,alp)
+
+    # Burrows-Wheeler
+    decoded_txt = decode_burrows_wheeler(mtf_decoded,index)
+
+    # Write decoded text to output file
+    write_decoded_text_to_file(decoded_txt)
+
+
+############
+### MAIN ###
+############
+
+def main():
+    
+    # Read input file
+    filename = sys.argv[1]
+    f = open(filename,'r',encoding='utf-8')
+    input_txt = f.readline()
+    f.close()
+
+    # Decode text and write it to file
+    decompressor(input_txt)
+
+if __name__ == "__main__":
+    main()
